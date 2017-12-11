320f40ce68a83aa250bf1040c30b5f9cd0508883
Vladislav Klimenko <sunsingerus@gmail.com>
2017-12-11 20:11:36 +0300

    block downloading geth by mist

--- mist-network-test.sh
+++ mist-network-test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 #mist --gethpath=$(which geth)
+
+# block downloading geth by Mist
+rm -rf ~/.config/Mist/binaries
+mkdir -m 000 ~/.config/Mist/binaries
+
+# use external geth
 mist --gethpath=$(which geth) --rpc=/home/user/.ethereum/geth.ipc --network=test --syncmode=fast
 
